e1ba2fe835c1a7e25cafd55a2130c54f3f3a0ce8
LokeshKiran <lokeshkskiran@gmail.com>
2022-12-02 17:55:04 +0530

    [Lokesh]Refactoring:Functional components with arrow functions

--- reactpracticeproblems/reactpracticeproblems/src/components/Greet.js
+++ reactpracticeproblems/reactpracticeproblems/src/components/Greet.js
@@ -1,5 +1,6 @@
 import React from 'react';
-function Greet(){
-    return <h2>Welcome to new World</h2>
-}
+// function Greet(){
+//     return <h2>Welcome to new World</h2>
+// }
+const Greet=()=> <h2>Welcome to new World</h2>
 export default Greet;
--- reactpracticeproblems/reactpracticeproblems/src/components/Wishes.js
+++ reactpracticeproblems/reactpracticeproblems/src/components/Wishes.js
@@ -1,6 +1,7 @@
  import React from 'react';
  //functional component
- function Wishes(){
-    return <h1>Hi Lokesh</h1>
- }
+//  function Wishes(){
+//     return <h1>Hi Lokesh</h1>
+//  }
+const Wishes=()=> <h1>Hii, Lokesh K S</h1>
  export default Wishes;
